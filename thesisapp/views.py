2b4001e4710e98a444520587b2ea404e59df639e
sandyhabala <132645411+sandyhabala@users.noreply.github.com>
2024-02-25 16:09:27 +0800

    add paginator

--- thesisapp/views.py
+++ thesisapp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Thesis
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     thesis_list = Thesis.objects.all()
+    paginator = Paginator(thesis_list, 5)
+    page_number = request.GET.get("page", 1)
+    thesis_list = paginator.page(page_number)
 
-    return render(request, "index.html", {"thesis_list": thesis_list})
+    return render(request, "post/list.html", {"thesis_list": thesis_list})
 
 
 def detail(request, id):
@@ -16,6 +19,6 @@ def detail(request, id):
 
     return render(
         request,
-        "detail.html",
+        "post/detail.html",
         {"detail": detail, "authors": authors, "panelists": panelists},
     )
